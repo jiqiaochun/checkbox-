b6986c837db2c142d1cf0663fbd4d44a58e9a525
@jiqiaochun <13011251196@163.com>
2016-11-04 10:12:46 +0800

    Initial Commit

---
+++ JYtestCheckBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JYtestCheckBox
+//
+//  Created by 姬巧春 on 2016/11/4.
+//  Copyright © 2016年 姬巧春. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JYtestCheckBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JYtestCheckBox
+//
+//  Created by 姬巧春 on 2016/11/4.
+//  Copyright © 2016年 姬巧春. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
